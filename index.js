c716c187cdedda248cc6deec7a93294d098cc075
theyoojeonglee <theyoojeonglee@gmail.com>
2022-06-21 19:22:58 +0900

    Set camera movement

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const mobile = express();
 
 const plantData = {};
 
+let flag = false;
+let message;
+let moveNum = 0;
+let moveDirection;
+
 arduino.use(bodyParser.json());
 arduino.use(bodyParser.urlencoded({ extended: true }));
 
@@ -20,45 +25,81 @@ mobile.listen(4000, () => {
    console.log(`Listening to PORT: 4000`);
 })
 
-arduino.post('/plant', (req, res) => {
-   plantData.plantID = req.body.plantID;
-   plantData.humidity = req.body.humidity;
-   plantData.temperature = req.body.temperature;
-   plantData.pH = req.body.pH;
-   plantData.sunlight = req.body.sunlight;
-   plantData.cameraPosition = req.body.cameraPosition;
+arduino.post('/', (req, res) => {
+   if(req.body.cameraPosition) {
+      plantData.cameraPosition = req.body.cameraPosition;
+   }
+   else {
+      plantData.humidity = req.body.humidity;
+      plantData.temperature = req.body.temperature;
+      plantData.ambientHumidity = req.body.ambientHumidity;
+      plantData.ph = req.body.ph;
+      plantData.sun = req.body.sun;
+      plantData.waterTank = req.body.waterTank;
+   }
    res.end('ok');
 })
+
+// save
+// 계속 위치 받음, 앱이 위치 달라고 하면 서버가 준다.
+
+
+/**
+ * 1. 앱이 current position 달라고 하면 주기
+ * 2. 앱이 이 식물 포지션으로 가라고 하면 해당 포지션에 따라서 왼쪽 오른쪽으로 가라고 아두이노에게 명령
+ *
+ */
 
 mobile.get('/plant', (req, res) => {
    res.send(plantData);
 })
 
 mobile.get('/water', (req, res) => {
-   router.post('https://localhost:5000/water', (innerReq, innerRes) => {
+   router.post('https://ip-of-arduino:5000/water', (innerReq, innerRes) => { //router or arduino
       innerRes.send({ "message": "waterCommand" });
       innerRes.end('ok');
    })
    res.end('ok');
 })
 
-mobile.post('/camera/direction', (req, res) => {
-   router.post('https://localhost:5000/camera/direction', (innerReq, innerRes) => {
-      innerRes.send({ "message": req.body.message + "\nStop\n", "success": "true" });
-      innerRes.end('ok');
-   })
-   res.end('ok');
+arduino.get('/', async (req, res) => {
+   if(flag) {
+      try {
+         console.log(flag, message);
+         flag = false;
+         res.send({"message": message, "success": "true"});
+      } catch (err) {
+         console.log(err);
+      }
+   }
+   else if(moveNum > 0){
+      console.log('movenum');
+      console.log(moveNum);
+      moveNum -= 2;
+      res.send({"message": moveDirection, "success": "true"});
+   }
+   else {
+      res.end('ok');
+      moveNum = 0;
+   }
+})
+
+mobile.post('/camera/position', (req, res) => {
+   flag = true;
+   message = req.body.control;
+   res.send('ok');
 })
 
 mobile.post('/camera/location', (req, res) => {
-   router.post('https://localhost:5000/camera/location', (innerReq, innerRes) => {
-      innerRes.send({ "message": req.body.message, "success": "true" });
-      innerRes.end('ok');
-   })
+   moveNum = Math.abs(req.body.cameraPosition - plantData.cameraPosition);
+   if(req.body.cameraPosition > plantData.cameraPosition) moveDirection = "camLeft";
+   else moveDirection = "camRight";
    res.end('ok');
 })
 
 mobile.put('/plant', (req, res) => {
-   plantData.cameraPosition = req.body.cameraPosition;
+   if(req.body.plantID == '1') plantData.cameraPosition1 = req.body.cameraPosition;
+   else plantData.cameraPosition2 = req.body.cameraPosition;
+   console.log(plantData);
    res.end('ok');
 })
